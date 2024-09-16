f41476b18de1f4934e79bc0bcded10b7a0ba9540
tseeganov <tseeganov@DESKTOP-FS86H3N>
2024-09-16 14:58:44 +1000

    начало pc2

--- 002_des/ecb.cpp
+++ 002_des/ecb.cpp
@@ -1,13 +1,14 @@
 #include <windows.h>
 #include <winDNS.h>
 
-BYTE* k1_buffer;
+
 QWORD get_bit(QWORD bit, int sh)
 {
 	int rsh = 64 - sh - 1;
 	return bit << rsh;
 }
 
+// неправильно для 1 бита?
 QWORD pc1(QWORD* hKey)
 {
 	QWORD pc1_key = 0;
@@ -22,20 +23,20 @@ QWORD pc1(QWORD* hKey)
 	pc1_key = pc1_key | ((*hKey & 0x4000000000000000) >> 6 );		// 1
 	pc1_key = pc1_key | ((*hKey & 0x20) << 58 - 8);					// 58
 	pc1_key = pc1_key | ((*hKey & 0x2000) << 50 - 9);				// 50
-	pc1_key = pc1_key | ((*hKey & 0x200000) << 42 - 10);				// 42
+	pc1_key = pc1_key | ((*hKey & 0x200000) << 42 - 10);			// 42
 	pc1_key = pc1_key | ((*hKey & 0x20000000) << 34 - 11);			// 34
-	pc1_key = pc1_key | ((*hKey & 0x2000000000) << 26 - 12);			// 26
+	pc1_key = pc1_key | ((*hKey & 0x2000000000) << 26 - 12);		// 26
 	pc1_key = pc1_key | ((*hKey & 0x200000000000) << 18 - 13);		// 18
 
 	pc1_key = pc1_key | ((*hKey & 0x20000000000000) >> 4);			// 10
 	pc1_key = pc1_key | ((*hKey & 0x2000000000000000) >> 13);		// 2
-	pc1_key = pc1_key | ((*hKey & 0x10) << 59 - 16);					// 59
+	pc1_key = pc1_key | ((*hKey & 0x10) << 59 - 16);				// 59
 	pc1_key = pc1_key | ((*hKey & 0x1000) << 51 - 17);				// 51
-	pc1_key = pc1_key | ((*hKey & 0x100000) << 43 - 18);				// 43
+	pc1_key = pc1_key | ((*hKey & 0x100000) << 43 - 18);			// 43
 	pc1_key = pc1_key | ((*hKey & 0x10000000) << 35 - 19);			// 35
-	pc1_key = pc1_key | ((*hKey & 0x1000000000) << 27 - 20);			// 27
+	pc1_key = pc1_key | ((*hKey & 0x1000000000) << 27 - 20);		// 27
 
-	pc1_key = pc1_key | ((*hKey & 0x100000000000) >> 2);				// 19
+	pc1_key = pc1_key | ((*hKey & 0x100000000000) >> 2);			// 19
 	pc1_key = pc1_key | ((*hKey & 0x10000000000000) >> 11);			// 11
 	pc1_key = pc1_key | ((*hKey & 0x1000000000000000) >> 20);		// 3
 	pc1_key = pc1_key | ((*hKey & 0x8) << 60 - 24);					// 60
@@ -48,32 +49,32 @@ QWORD pc1(QWORD* hKey)
 	pc1_key = pc1_key | ((*hKey & 0x10000) << 47 - 30);				// 47
 	pc1_key = pc1_key | ((*hKey & 0x1000000) << 39 - 31);			// 39
 	pc1_key = pc1_key | ((*hKey & 0x100000000) >> 1);				// 31
-	pc1_key = pc1_key | ((*hKey & 0x10000000000) >> 10);				// 23
+	pc1_key = pc1_key | ((*hKey & 0x10000000000) >> 10);			// 23
 	pc1_key = pc1_key | ((*hKey & 0x1000000000000) >> 19);			// 15
 
-	pc1_key = pc1_key | ((*hKey & 0x100000000000000) >> 28);			// 7
+	pc1_key = pc1_key | ((*hKey & 0x100000000000000) >> 28);		// 7
 	pc1_key = pc1_key | ((*hKey & 0x2) << 62 - 36);					// 62
 	pc1_key = pc1_key | ((*hKey & 0x200) << 54 - 37);				// 54
 	pc1_key = pc1_key | ((*hKey & 0x20000) << 46 - 38);				// 46
 	pc1_key = pc1_key | ((*hKey & 0x2000000) >> 1);					// 38
 	pc1_key = pc1_key | ((*hKey & 0x200000000) >> 10);				// 30
-	pc1_key = pc1_key | ((*hKey & 0x20000000000) >> 19);				// 22
+	pc1_key = pc1_key | ((*hKey & 0x20000000000) >> 19);			// 22
 
 	pc1_key = pc1_key | ((*hKey & 0x2000000000000) >> 28);			// 14
-	pc1_key = pc1_key | ((*hKey & 0x200000000000000) >> 37);			// 6
+	pc1_key = pc1_key | ((*hKey & 0x200000000000000) >> 37);		// 6
 	pc1_key = pc1_key | ((*hKey & 0x4) << 61 - 44);					// 61
 	pc1_key = pc1_key | ((*hKey & 0x400) << 53 - 45);				// 53
 	pc1_key = pc1_key | ((*hKey & 0x40000) >> 1);					// 45
-	pc1_key = pc1_key | ((*hKey & 0x4000000) >> 10);					// 37
+	pc1_key = pc1_key | ((*hKey & 0x4000000) >> 10);				// 37
 	pc1_key = pc1_key | ((*hKey & 0x400000000) >> 19);				// 29
 
-	pc1_key = pc1_key | ((*hKey & 0x40000000000) >> 28);				// 21
+	pc1_key = pc1_key | ((*hKey & 0x40000000000) >> 28);			// 21
 	pc1_key = pc1_key | ((*hKey & 0x4000000000000) >> 37);			// 13
-	pc1_key = pc1_key | ((*hKey & 0x400000000000000) >> 46);			// 5
+	pc1_key = pc1_key | ((*hKey & 0x400000000000000) >> 46);		// 5
 	pc1_key = pc1_key | ((*hKey & 0x800000000) >> 24);				// 28
-	pc1_key = pc1_key | ((*hKey & 0x80000000000) >> 33);				// 20
+	pc1_key = pc1_key | ((*hKey & 0x80000000000) >> 33);			// 20
 	pc1_key = pc1_key | ((*hKey & 0x8000000000000) >> 42);			// 12
-	pc1_key = pc1_key | ((*hKey & 0x800000000000000) >> 51);			// 4	
+	pc1_key = pc1_key | ((*hKey & 0x800000000000000) >> 51);		// 4	
 	return pc1_key;
 }
 
@@ -102,8 +103,8 @@ void make_shift(DWORD* hH, DWORD* hL, int i)
 		*hH = *hH << 2;
 	}
 }
-
-void make_pc2(DWORD h, DWORD l)
+BYTE* k1_buffer;
+void make_pc2(DWORD h, DWORD l, int i)
 {
 	// объединяем h и l
 	QWORD hlUnion = 0;
@@ -114,9 +115,17 @@ void make_pc2(DWORD h, DWORD l)
 	l_tmp = l_tmp << 4;
 	hlUnion = hlUnion | l_tmp;
 	
+	*(k1_buffer + i) = hlUnion & 0x2000000000000;							// 14
+	*(k1_buffer + i) = *(k1_buffer + i) | hlUnion & 0x400000000000;			// 17
+	*(k1_buffer + i) = *(k1_buffer + i) | hlUnion & 0x10000000000000;		// 11
+	*(k1_buffer + i) = *(k1_buffer + i) | hlUnion & 0x8000000000;			// 24	
+	*(k1_buffer + i) = *(k1_buffer + i) | hlUnion & 0x4000000000000000;		// 1
+	*(k1_buffer + i) = *(k1_buffer + i) | hlUnion & 0x400000000000000;		// 5
+
 
 
 }
+
 
 void make_k1(QWORD pc1)
 {
@@ -131,7 +140,7 @@ void make_k1(QWORD pc1)
 	while (i < 16)
 	{
 		make_shift(&h, &l, i);
-		make_pc2(h, l);
+		make_pc2(h, l, i);
 		i++;
 	}
 
--- bit_helper/Program.cs
+++ bit_helper/Program.cs
@@ -10,15 +10,27 @@ namespace bit_helper
     {
         static void Main(string[] args)
         {
-            while (true)
+
+           while (true)
             {
-                int n;
-                string x = "1";
-                n = Convert.ToInt32(Console.ReadLine());
-                string str = "0000000000000000000000000000000000000000000000000000000000000000";
-                str = str.Substring(n + 1);
-                Console.WriteLine(x + str);
+                Console.WriteLine("Введите порядковый номер бита (от 0 до 63): ");
+                int bitIndex = int.Parse(Console.ReadLine());
+
+                // Проверка на корректность ввода
+                if (bitIndex < 0 || bitIndex > 63)
+                {
+                    Console.WriteLine("Ошибка: введите число от 0 до 63.");
+                    return;
+                }
+
+                // Создаем число, где только один бит установлен на 1 в указанной позиции
+                ulong bitMask = 1UL << bitIndex;
+
+                // Выводим число в шестнадцатеричном формате
+                Console.WriteLine($"Число, соответствующее биту {bitIndex}, в шестнадцатеричном формате: {bitMask:X}");
             }
+           
+            
         }
     }
 }
